4a80e0645a184ba98a2f83f117aee5dc18e101b5
wReinerLueth <wlueth@usc.edu>
2017-11-27 09:01:09 -0800

    asdfd

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,5 +21,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');  
+
   return app.toTree();
 };
