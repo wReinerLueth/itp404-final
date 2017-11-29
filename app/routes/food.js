506d0fce6215d6c492e437df83310d44e7938b3a
wReinerLueth <wlueth@usc.edu>
2017-11-29 14:55:24 -0800

    bootstrap is back!!!! main div is weird, under the navbar. dropdowns don't work

--- app/routes/food.js
+++ app/routes/food.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Route.extend({
+    title: 'Food Near Me',
     actions: {
         refresh(){
+            // This only gets the div, not the map itself
             let map = $('#mainMap');
             console.log(map);
         }
