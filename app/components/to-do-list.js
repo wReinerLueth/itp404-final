fd45f9b5cbd733acdb92e4a994129afe7fe70bff
wReinerLueth <wlueth@usc.edu>
2017-11-30 16:03:03 -0800

    adding list model

--- app/components/to-do-list.js
+++ app/components/to-do-list.js
@@ -1,5 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    
+    model() {
+        return this.store.findAll('checklistItem')
+    },
+    actions: {
+        createInstructions(e) {
+            e.preventDefault(); // if I decide to use a form
+            let hour = this.get('hour');
+            let min = this.get('min');
+            let time;
+
+            if (min == 30) {
+                time = hour + .5;
+            }
+            else {
+                time = hour;
+            }
+
+            if (hour > 12){
+                time -= 12;
+                time += 'pm';
+            }
+            else if (hour == 12){
+                time += 'pm';
+            }
+            else{
+                time += 'am';
+            }
+
+            this.store.createRecord('checklistItem', {
+                instruction: this.get('instruction'),
+                time: time
+            });
+
+            
+        }
+    }
 });
