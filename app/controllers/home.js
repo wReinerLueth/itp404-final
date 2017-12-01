bfd117e017295f2bac613ddc70487fd34068afd8
wReinerLueth <wlueth@usc.edu>
2017-11-30 18:49:13 -0800

    toastr in there, shouldn't be broken lol

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -10,7 +10,39 @@ export default Ember.Controller.extend({
                 time: this.get('time')
             });
 
+            this.toast.success("You added the task");
             todo.save();
+            
+        },
+        deleteItem(){
+
         }
     }
 });
+
+// Sorting
+
+// var lastLetterSort = ['blue', 'red', 'green', 'aa'];
+
+
+// var sorted = lastLetterSort.sort(function(a, b) {
+    
+//         if (a[a.length - 1] > b[b.length - 1])
+//             return 1;
+//         else if (a[a.length - 1] < b[b.length - 1])
+//             return -1;
+    
+//         return 0;
+    
+//     })
+    
+//     console.log(sorted)
+
+
+
+// Toastr stuff
+
+// miniToastr.success(message, title, timeout, cb, config)
+// miniToastr.info(message, title, timeout, cb, config)
+// miniToastr.warn(message, title, timeout, cb, config)
+// miniToastr.error(message, title, timeout, cb, config)
